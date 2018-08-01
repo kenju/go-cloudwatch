92d6fce91b1611231e261fe706a0aa0498d87483
kenju <ken901waga@gmail.com>
2018-08-01 15:59:01 +0900

    remove package name from struct

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package cloudwatch
 
-// CloudWatchAlarmMessage is a message JSON sent from CloudWatch Alarm via SNS Topic.
-type CloudWatchAlarmMessage struct {
-	AlarmName        string                 `json:"AlarmName"`
-	AlarmDescription string                 `json:"AlarmDescription"`
-	AWSAccountID     string                 `json:"AWSAccountId"`
-	NewStateValue    string                 `json:"NewStateValue"`
-	NewStateReason   string                 `json:"NewStateReason"`
-	StateChangeTime  string                 `json:"StateChangeTime"`
-	Region           string                 `json:"Region"`
-	OldStateValue    string                 `json:"OldStateValue"`
-	Trigger          CloudWatchAlarmTrigger `json:"Trigger"`
+// AlarmMessage is a message JSON sent from CloudWatch Alarm via SNS Topic.
+type AlarmMessage struct {
+	AlarmName        string       `json:"AlarmName"`
+	AlarmDescription string       `json:"AlarmDescription"`
+	AWSAccountID     string       `json:"AWSAccountId"`
+	NewStateValue    string       `json:"NewStateValue"`
+	NewStateReason   string       `json:"NewStateReason"`
+	StateChangeTime  string       `json:"StateChangeTime"`
+	Region           string       `json:"Region"`
+	OldStateValue    string       `json:"OldStateValue"`
+	Trigger          AlarmTrigger `json:"Trigger"`
 }
 
-// CloudWatchAlarmTrigger is a JSON of CloudWatchAlarmMessage.
-type CloudWatchAlarmTrigger struct {
-	MetricName                       string                     `json:"MetricNameOldStateValue"`
-	Namespace                        string                     `json:"Namespace"`
-	StatisticType                    string                     `json:"StatisticType"`
-	Unit                             int                        `json:"Unit"`
-	Dimensions                       []CloudWatchAlarmDimension `json:"Dimensions"`
-	Period                           int                        `json:"period"`
-	EvaluationPeriods                int                        `json:"EvaluationPeriods"`
-	ComparisonOperator               string                     `json:"ComparisonOperator"`
-	Threshold                        int                        `json:"Threshold"`
-	TreatMissingData                 string                     `json:"TreatMissingData"`
-	EvaluateLowSampleCountPercentile string                     `json:"EvaluateLowSampleCountPercentile"`
+// AlarmTrigger is a JSON of CloudWatchAlarmMessage.
+type AlarmTrigger struct {
+	MetricName                       string           `json:"MetricNameOldStateValue"`
+	Namespace                        string           `json:"Namespace"`
+	StatisticType                    string           `json:"StatisticType"`
+	Unit                             int              `json:"Unit"`
+	Dimensions                       []AlarmDimension `json:"Dimensions"`
+	Period                           int              `json:"period"`
+	EvaluationPeriods                int              `json:"EvaluationPeriods"`
+	ComparisonOperator               string           `json:"ComparisonOperator"`
+	Threshold                        int              `json:"Threshold"`
+	TreatMissingData                 string           `json:"TreatMissingData"`
+	EvaluateLowSampleCountPercentile string           `json:"EvaluateLowSampleCountPercentile"`
 }
 
-// CloudWatchAlarmDimension is a JSON of CloudWatchAlarmTrigger.
-type CloudWatchAlarmDimension struct {
+// AlarmDimension is a JSON of CloudWatchAlarmTrigger.
+type AlarmDimension struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
